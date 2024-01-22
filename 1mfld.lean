f2e252413147af50cbca891403ddd240c6c77749
Jim Fowler <fowler@math.osu.edu>
2024-01-22 13:17:32 -0500

    real line is a manifold

--- 1mfld.lean
+++ 1mfld.lean
@@ -1,4 +1,14 @@
 import Mathlib
 
+-- class TopologicalManifold (n : ℕ) (X : Type u) ... ?
+
 class Topological1Manifold (X : Type u) [TopologicalSpace X] [T2Space X] : Prop where
   locally_euclidean : ∀ ⦃x : X⦄, ∃ (h : (PartialHomeomorph X ℝ)), x ∈ h.source
+
+instance : Topological1Manifold ℝ := {
+  locally_euclidean := by
+    intro x
+    have h' : Homeomorph ℝ ℝ := Homeomorph.refl ℝ
+    use Homeomorph.toPartialHomeomorph h'
+    simp only [Homeomorph.toPartialHomeomorph_source, Set.mem_univ]
+}
