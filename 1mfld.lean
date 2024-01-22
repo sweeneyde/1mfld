f26399fcefe55e6ee19e117cd7a90689f172851f
Jim Fowler <fowler@math.osu.edu>
2024-01-22 12:45:59 -0500

    definition of topological 1-manifold

--- 1mfld.lean
+++ 1mfld.lean
@@ -1,1 +1,4 @@
-def hello := "world"
+import Mathlib
+
+class Topological1Manifold (X : Type u) [TopologicalSpace X] [T2Space X] : Prop where
+  locally_euclidean : ∀ ⦃x : X⦄, ∃ (h : (PartialHomeomorph X ℝ)), x ∈ h.source
